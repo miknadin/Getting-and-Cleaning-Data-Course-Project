e539b23cfb66d78a0b299800cb5f2b5db86a0a0a
miknadin <miknadin@users.noreply.github.com>
2015-07-10 00:22:13 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -29,3 +29,26 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", sep = "",
 subject <- rbind(subject_train, subject_test)
 Data_X <- cbind(Data_X, subject)
 
+txt_mean <- Data_X[grep("mean()", names(Data_X), fixed = TRUE)]
+txt_std <- Data_X[grep("std()", names(Data_X), fixed = TRUE)]
+txt <- cbind(txt_mean, txt_std, Data_X[,562:563])
+names(txt)[67] <- "label_activity"
+names(txt)[68] <- "object"
+
+txt$activity[txt$label_activity == 1] <- "walking"           #step 3
+txt$activity[txt$label_activity == 2] <- "walking upstairs"
+txt$activity[txt$label_activity == 3] <- "walking downstairs"
+txt$activity[txt$label_activity == 4] <- "sitting"
+txt$activity[txt$label_activity == 5] <- "staying"
+txt$activity[txt$label_activity == 6] <- "laying"
+
+df <- data.frame()
+df <- aggregate(x = txt[,1:66], FUN = "mean", by = list(txt$activity, txt$object))
+
+names(df)[1] <- "activity"
+names(df)[2] <- "object"
+
+write.table(df, file = "step5.txt", row.name=FALSE)
+
+data <- read.table("./step5.txt", header = TRUE)
+
